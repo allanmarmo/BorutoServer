9a9f112642a80cead9896d3a2f3c94a885a693e3
allanmarmo <allanmarmo@gmail.com>
2021-12-16 15:21:56 -0300

    Initial commit 14. Setup the Project - Server

---
+++ src/main/kotlin/br/com/webmarmo/Application.kt
@@ -0,0 +1,14 @@
+package br.com.webmarmo
+
+import io.ktor.application.*
+import br.com.webmarmo.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
